71755d75e76b49670846124c6dd5bc310027bb67
Manthan <manthansturakhia@gmail.com>
2020-09-27 12:13:16 +0530

    Frontend

--- codedaddies_list-master/my_app/admin.py
+++ codedaddies_list-master/my_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
+from .models import Search
 
 # Register your models here.
-from . models import Search
-
 admin.site.register(Search)
--- codedaddies_list/codedaddies_list/my_app/views.py
+++ codedaddies_list/codedaddies_list/my_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, template_name = 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend={
+        'search': search,
+
+    }
+    return render(request, template_name = 'my_app/new_search.html',context = stuff_for_frontend)
